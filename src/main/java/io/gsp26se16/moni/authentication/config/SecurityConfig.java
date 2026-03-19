437f3192d1bf010001dde5a2e127d16e9a56a8ed
Hoang Vu <vuhse182692@fpt.edu.vn>
2026-03-19 14:32:35 +0700

    update fe domain

--- src/main/java/io/gsp26se16/moni/authentication/config/SecurityConfig.java
+++ src/main/java/io/gsp26se16/moni/authentication/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://moni-fe.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
