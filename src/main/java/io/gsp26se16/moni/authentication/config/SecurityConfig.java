67a18dfa3dc66629aa208ee31cadffdbf5b5c3ea
Vu Hoang <vuhse182692@fpt.edu.vn>
2026-03-22 10:31:54 +0700

    fix access sepay endpoint

--- src/main/java/io/gsp26se16/moni/authentication/config/SecurityConfig.java
+++ src/main/java/io/gsp26se16/moni/authentication/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -53,12 +52,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Cho phép tất cả
 
         return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers("/payments/sepay");
     }
 
     @Bean
