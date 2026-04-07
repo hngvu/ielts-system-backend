e6bbfedf4e6fd85533838e8afb7d16b2f5567eed
iamnotadev <qlv2810@gmail.com>
2026-04-08 00:55:14 +0700

    mfix writijg

--- src/main/java/io/gsp26se16/moni/content/service/TestServiceImpl.java
+++ src/main/java/io/gsp26se16/moni/content/service/TestServiceImpl.java
@@ -35,6 +35,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TestServiceImpl implements TestService {
 
     private final TestRepository testRepository;
