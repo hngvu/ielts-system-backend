2a41810084b81719c182036ae0dcd6065c0d5d1d
Hoang Vu <vuhse182692@fpt.edu.vn>
2026-04-07 17:56:05 +0700

    fix dto

--- src/main/java/io/gsp26se16/moni/content/dto/request/QuestionUpdateRequest.java
+++ src/main/java/io/gsp26se16/moni/content/dto/request/QuestionUpdateRequest.java
@@ -3,12 +3,16 @@ package io.gsp26se16.moni.content.dto.request;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import lombok.Data;
 
 @Data
 public class QuestionUpdateRequest {
     private String content;
     private Map<String, Object> explanation;
+
+    @JsonAlias("orderIndex")
     private Integer position;
 
     private List<Integer> tagIds;
