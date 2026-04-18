c5a14f5dcb8f333385ee3915d8340f419daa0018
Le Quan <qlv2810@gmail.com>
2026-04-18 13:38:13 +0700

    Fix: Jackson deserialization error (Missing default constructor) when parsing VocabQuizHistory data

--- src/main/java/io/gsp26se16/moni/vocab/dto/QuizQuestion.java
+++ src/main/java/io/gsp26se16/moni/vocab/dto/QuizQuestion.java
@@ -2,11 +2,15 @@ package io.gsp26se16.moni.vocab.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuizQuestion {
     Integer id;
     String type;
